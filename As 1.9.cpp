0416ead1830ced87ff27ff2c12ca7d220529dc63
Narasingdharani <130465865+Narasingdharani@users.noreply.github.com>
2023-04-11 20:14:40 +0530

    Add files via upload

---
+++ As 1.9.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num, rev = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (; num != 0; num /= 10) {
+        rev = rev * 10 + num % 10;
+    }
+
+    printf("Reverse of %d = %d", num, rev);
+
+    return 0;
+}
+
